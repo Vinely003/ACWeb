c08851de1890fd2d7b0debe2063b52434e11c3a5
Vinely003 <89468885+Vinely003@users.noreply.github.com>
2023-06-25 18:51:35 +0200

    Add files via upload

--- AC/moduls/belepes.php
+++ AC/moduls/belepes.php
@@ -1,6 +1,8 @@
 <?php
-if ($_GET["regisztralva"] == "igaz") {
-    header("Location:./moduls/fo_oldal.php");
-} elseif ($_GET["regisztralva"] == "hamis") {
-    header("Location:./index.php");
+if (isset($_GET["regisztralva"])) {
+    if ($_GET["regisztralva"] == "igaz") {
+        header("Location:./moduls/fo_oldal.php");
+    } elseif ($_GET["regisztralva"] == "hamis") {
+        header("Location:./index.php");
+    }
 }
--- AC/moduls/reg.php
+++ AC/moduls/reg.php
@@ -1,8 +1,10 @@
 <form action="./moduls/reg_ment.php" method="POST">
     <?php
     session_start();
-    if (count($_SESSION["reghiba"]) > 0) {
-        echo "<div class ='hiba'>" . implode($_SESSION["hiba"]) . "</div>";
+    if (isset($_SESSION["reghiba"])) {
+        if (count($_SESSION["reghiba"]) > 0) {
+            echo "<div class ='hiba'>" . implode($_SESSION["reghiba"]) . "</div>";
+        }
     }
     ?>
     <input type="text" name="nev" placeholder="Írj egy felhasználó nevet...">
@@ -11,5 +13,6 @@
     <br>
     <input type="submit" id="button" value="Megerősítés">
 </form>
-
-<a href="./moduls/be.php">Beljelentkezés</a>
+<div class=belepes>
+    <a href="./moduls/be.php">Bejelentkezés</a>
+</div>
